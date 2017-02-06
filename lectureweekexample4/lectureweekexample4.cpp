8e42b7b6c39cc58750d94edee03f35490138670d
joneswk <joneswk@mymail.vcu.edu>
2017-02-06 18:51:35 -0500

    HelloDD

--- lectureweekexample4/lectureweekexample4.cpp
+++ lectureweekexample4/lectureweekexample4.cpp
@@ -1,6 +1,37 @@
 // lectureweekexample4.cpp : Defines the entry point for the console application.
 //
 
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+
+	char fullname[50];
+	char *lastname;
+
+	// ask user for name
+	cout << "Please enter your full name" << endl;
+	gets_s(fullname);
+
+	lastname = fullname;  // == lastname = &fullname[0];
+	cout << lastname << endl;
+
+	int i = 0;
+	while (fullname[i] != ' ')
+	{
+		lastname++;
+		i++;
+	}
+	lastname++;
+	cout << lastname << endl;
+
+	return 0;
+}
 #include "stdafx.h"
 
 
